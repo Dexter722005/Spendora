60b64f47bfd23c723f267444a5437d1d887c2e79
Dexter722005 <shreshthpanda7@gmail.com>
2026-04-14 21:59:49 +0530

    fix root route

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/transactions", require("./routes/transactionRoutes"));
